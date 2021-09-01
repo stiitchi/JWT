c70409bc45bd9928d9483810135de651b063ab35
Ruan Nigrini <62988609+RecursiveRuan@users.noreply.github.com>
2021-09-01 17:52:48 +0200

    Add project files.

---
+++ WebApplication1/Controllers/SecuredController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecuredController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetSecuredData()
+        {
+            return Ok("This Secured Data is available only for Authenticated Users.");
+        }
+
+        //Here we only give administrator permission 
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> PostSecuredData()
+        {
+            return Ok("This Secured Data is available only for Authenticated Users.");
+        }
+    }
+}
---
+++ WebApplication1/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public interface IUserService
+    {
+        Task<string> RegisterAsync(RegisterModel model);
+        Task<AuthenticationModel> GetTokenAsync(TokenRequestModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
+    } 
+}
